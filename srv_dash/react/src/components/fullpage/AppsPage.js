6a6c80a0a297f9693ff922848f978a1071fee51e
Baran Kucukguzel <baran@scorebeyond.com>
2016-11-13 21:22:57 +0300

    client initialized with react-router

---
+++ srv_dash/react/src/components/fullpage/AppsPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class AppsPage extends React.Component {
+    render() {
+        return (
+            <div>
+                Apps Page
+            </div>
+        )
+    }
+}
--- srv_dash/react/src/index.js
+++ srv_dash/react/src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class World extends React.Component {
-  render() {
-    return <h1>World</h1>
-  }
+import { Router, Route, hashHistory, IndexRoute, browserHistory } from 'react-router';
+import RedBox from 'redbox-react';
+
+import DashContainer from './components/fullpage/DashContainer';
+import AppsPage from './components/fullpage/AppsPage';
+
+class DashApp extends React.Component {
+    render() {
+        return (
+            <Router history={hashHistory}>
+                <Route path="/" component={DashContainer}>
+                    <IndexRoute component={AppsPage}/>
+                </Route>
+            </Router>
+        )
+    }
 }
 
-ReactDOM.render(<World/>, document.getElementById('app'));
+try {
+    ReactDOM.render(<DashApp />, document.getElementById('app'));
+} catch(e) {
+    console.log(e);
+    render(<RedBox error={e} />, document.getElementById('app'));
+}
