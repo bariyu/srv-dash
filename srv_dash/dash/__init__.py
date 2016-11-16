7021fac0dd98f13b11ce4fb76e791403a6cd20ac
Baran Kucukguzel <baran@scorebeyond.com>
2016-11-16 10:26:16 +0300

    98 percentile server response time metric added

--- srv_dash/dash/__init__.py
+++ srv_dash/dash/__init__.py
@@ -2,3 +2,4 @@
 # so that they are cathced in reflection phase in the factory
 import mt_avg_server_time
 import mt_path_hit_count
+import mt_98_perc_server_time
--- srv_dash/dash/mt_98_perc_server_time.py
+++ srv_dash/dash/mt_98_perc_server_time.py
@@ -1,15 +1,15 @@
 from base_metric import BaseMetric
 from constants import HTTP_SERVER_REQ_RESP_MEASUREMENT_NAME
 
-class AverageServerTimeMetric(BaseMetric):
+class Perc98ServerTimeMetric(BaseMetric):
     def __init__(self):
-        BaseMetric.__init__(self, 'avg_server_time')
+        BaseMetric.__init__(self, 'perc_98_server_time')
 
     def get_continious_query_body(self):
-        return 'SELECT mean("resp-time") INTO "{}" FROM {}'.format(self.name, HTTP_SERVER_REQ_RESP_MEASUREMENT_NAME)
+        return 'SELECT percentile("resp-time", 98) INTO "{}" FROM {}'.format(self.name, HTTP_SERVER_REQ_RESP_MEASUREMENT_NAME)
 
     def data_key(self):
-        return 'mean'
+        return 'percentile'
 
     def chart_type(self):
         return 'linear'
@@ -18,4 +18,4 @@ class AverageServerTimeMetric(BaseMetric):
         return 'ms'
 
     def metric_title(self):
-        return 'average server response time for all requests'
+        return '98th percentile server response time'
