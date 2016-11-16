233b466097326102e46d8a25fcdd1b1d5c9d28fa
Baran Kucukguzel <baran@scorebeyond.com>
2016-11-17 00:29:51 +0300

    typefix

--- srv_dash/react/src/reducers/index.js
+++ srv_dash/react/src/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux'
 import appReducer from './AppReducer';
-import dashReducer from './dashReducer';
+import dashReducer from './DashReducer';
 
 
 const rootReducer = combineReducers({
