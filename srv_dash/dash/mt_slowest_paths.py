71d9f1b8fd1753f6e21820df146620745cce9738
Baran Kucukguzel <baran@scorebeyond.com>
2016-11-17 19:05:43 +0300

    slowest path calculation update to be more accurate

--- srv_dash/dash/mt_slowest_paths.py
+++ srv_dash/dash/mt_slowest_paths.py
@@ -31,27 +31,27 @@ class SlowestPathsCountMetric(BaseMetric):
         return 'linear'
 
     def metric_title(self):
-        return 'slowest server time paths (most used {} paths)'.format(self.max_different_path())
+        return 'slowest server time paths (slowsest mean {} paths)'.format(self.max_different_path())
 
-    def sum_query(self, app=None):
+    def mean_query(self, app=None):
         if not app:
-            return 'SELECT SUM("mean") from {} GROUP BY "path"'.format(self.name)
-        return 'SELECT SUM("mean") from {} WHERE "app" = \'{}\' GROUP BY "path"'.format(self.name, app)
+            return 'SELECT MEAN("mean") from {} GROUP BY "path"'.format(self.name)
+        return 'SELECT MEAN("mean") from {} WHERE "app" = \'{}\' GROUP BY "path"'.format(self.name, app)
 
     def max_different_path(self):
         return 5
 
     def get_series(self, influx_client, app=None):
-        max_result_set = influx_client.query(self.sum_query(app=app))
+        max_result_set = influx_client.query(self.mean_query(app=app))
         items = max_result_set.items()
         path_sums = []
         for item in items:
-            path_sum_parsed = [data for data in item[1]][0]['sum']
+            path_sum_parsed = [data for data in item[1]][0]['mean']
             path_sums.append({
-                'sum': path_sum_parsed,
+                'mean': path_sum_parsed,
                 'path': item[0][1]['path'],
             })
-        path_sums = sorted(path_sums, key=lambda x: x['sum'], reverse=True)
+        path_sums = sorted(path_sums, key=lambda x: x['mean'], reverse=True)
         max_different_path = self.max_different_path()
         if max_different_path < len(path_sums):
             path_sums = path_sums[:max_different_path]
